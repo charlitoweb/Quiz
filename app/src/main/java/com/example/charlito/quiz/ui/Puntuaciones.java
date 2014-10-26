a0b46c1cb848d2b519f64fee96c4c4927bd3ca07
charlitoweb <cregueira2@gmail.com>
2014-10-26 08:11:23 +0100

    Se añade  un Spinner (desplegable en android) para la respuesta correcta, los valores que se muestran en el desplegable los recoge del fichero valores_pregunta_correcta.xml del directorio values

--- app/src/main/java/com/example/charlito/quiz/ui/Puntuaciones.java
+++ app/src/main/java/com/example/charlito/quiz/ui/Puntuaciones.java
@@ -1,6 +1,7 @@
 package com.example.charlito.quiz.ui;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,24 +18,23 @@ import java.util.ArrayList;
 public class Puntuaciones extends Activity {
     protected Button salirPuntuaciones;
     protected ListView listaPuntuaciones;
+    protected SharedPreferences prefs;
+    protected ArrayList<Lista_entrada> datos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listado_puntuaciones);
-//        SharedPreferences preferencias = getPreferences(MODE_PRIVATE);
-//        String nombre = preferencias.getString("nombre","valor por defecto");
-//        int edad = preferencias.getInt("edad", -1);
+        escribirPuntuaciones();
+        cargarPuntuaciones();
 
-        //listadoPuntuaciones
-        ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
-        datos.add(new Lista_entrada("Varela","1000"));
+        /*datos.add(new Lista_entrada(nombrePref ,Integer.toString(puntuacionPref)));
         datos.add(new Lista_entrada("Hector","800"));
         datos.add(new Lista_entrada("Carlos","250"));
         datos.add(new Lista_entrada("Varela","100"));
         datos.add(new Lista_entrada("Hector","90"));
         datos.add(new Lista_entrada("Hector","80"));
         datos.add(new Lista_entrada("Carlos","50"));
-        datos.add(new Lista_entrada("Hector","10"));
+        datos.add(new Lista_entrada("Hector","10"));*/
         listaPuntuaciones =(ListView)findViewById(R.id.listadoPuntuaciones);
         listaPuntuaciones.setAdapter(new Lista_adaptador(this, R.layout.activity_puntuaciones, datos){
             @Override
@@ -58,6 +58,38 @@ public class Puntuaciones extends Activity {
                 finish();
             }
         });*/
+    }
+    private void escribirPuntuaciones() {
+        prefs = getSharedPreferences("Puntuaciones",this.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        for (int n = 9; n >= 0; n--) {
+
+            editor.putString("puntuacion" + n,"Hector "+Integer.toString(n*100));
+        }
+       /* editor.putString("puntuacion0", puntos + " " + nombre);
+        editor.putInt("puntuacionPref",1000);
+        editor.putString("nombrePref", "Hector");*/
+        editor.commit();
+    }
+    private void cargarPuntuaciones() {
+        String [] puntuaciones = new String[10];
+        String [] nombrePref = new String[10];
+        String [] puntuacionPref = new String[10];
+        for (int n = 9; n >= 0; n--) {
+            puntuaciones[n] = prefs.getString("puntuacion"+n,"");
+            String[]result = puntuaciones[n].split("\\s");
+            nombrePref[n]=result[0];
+            puntuacionPref[n]=result[1];
+            //String nombrePref = prefs.getString("nombrePref", "valor por defecto");
+            //int puntuacionPref = prefs.getInt("puntuacionPref", -1);
+        }
+        //listadoPuntuaciones
+        datos = new ArrayList<Lista_entrada>();
+        for (int n = 9; n >= 0; n--) {
+            datos.add(new Lista_entrada(nombrePref[n] ,puntuacionPref[n]));
+        }
     }
 
 }
--- app/src/main/java/com/example/charlito/quiz/ui/SendMailActivity.java
+++ app/src/main/java/com/example/charlito/quiz/ui/SendMailActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.charlito.quiz.R;
@@ -21,12 +24,37 @@ public class SendMailActivity extends Activity {
     private TextView opcion3Text;
     private TextView opcion4Text;
     private TextView opcionVerdaderaText;
+    private Spinner cmbOpciones;
+    private int verd; //opcion seleccionada del spinner
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_send_mail);
 		final Button send = (Button) this.findViewById(R.id.button1);
         final Button salir = (Button) this.findViewById(R.id.salirPreg);
+        //Definimos el adaptador para el spinner
+        final ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this,R.array.valores_pregunta_correcta,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);//Enlazamos nuestro spinner
+        cmbOpciones = (Spinner)findViewById(R.id.CmbOpciones);
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        cmbOpciones.setAdapter(adapter);
+
+        cmbOpciones.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent,
+                                               android.view.View v, int position, long id) {
+                        //lblMensaje.setText("Seleccionado: " + datos[position]);
+                        verd = position + 1;
+                    }
+
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        verd = 1;
+                    }
+                });
+
             salir.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     finish();
@@ -46,14 +74,30 @@ public class SendMailActivity extends Activity {
                 opcion2Text = (TextView) findViewById(R.id.opcion2Text);
                 opcion3Text = (TextView) findViewById(R.id.opcion3Text);
                 opcion4Text = (TextView) findViewById(R.id.opcion4Text);
-                opcionVerdaderaText = (TextView) findViewById(R.id.opcionVerdaderaText);
+                //opcionVerdaderaText = (TextView) findViewById(R.id.opcionVerdaderaText);
+
+
+
+
+
+
 
                 String emailBody = "Pregunta: "+preguntaText.getText().toString();
                 emailBody = emailBody + "<br>Opcion 1: " +opcion1Text.getText().toString();
                 emailBody = emailBody + "<br>Opcion 2: " +opcion2Text.getText().toString();
                 emailBody = emailBody + "<br>Opcion 3: " +opcion3Text.getText().toString();
                 emailBody = emailBody + "<br>Opcion 4: " +opcion4Text.getText().toString();
-                emailBody = emailBody + "<br>Opcion Correcta: " +opcionVerdaderaText.getText().toString();
+                /*String verd = CmbOpciones.getText().toString();
+                if (verd == "Opcion 1"){
+                    verd = "1";
+                }else if(verd == "Opcion 2"){
+                    verd = "2";
+                }else if (verd == "Opcion 3"){
+                    verd = "3";
+                }else{
+                    verd = "4";
+                }*/
+                emailBody = emailBody + "<br>Opcion Correcta: " +verd;
                 new SendMailTask(SendMailActivity.this).execute(fromEmail,
                         fromPassword, toEmailList, "nueva pregunta de Preguntas Freak", emailBody);
                 borraDatos();
@@ -67,6 +111,6 @@ public class SendMailActivity extends Activity {
         opcion2Text.setText("");
         opcion3Text.setText("");
         opcion4Text.setText("");
-        opcionVerdaderaText.setText("");
+//        opcionVerdaderaText.setText("");
     }
 }
